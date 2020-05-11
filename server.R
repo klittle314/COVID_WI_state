7f23f0bfbe57aa970efe145981d06c78625754c4
klittle314 <klittle@iecodesign.com>
2020-05-10 20:55:52 -0500

    add reactive function make_control_charts; add renderUI object for slope chart object; add renderPlot and renderUI functions for the control chart objects;

--- server.R
+++ server.R
@@ -12,6 +12,8 @@ source("global.R")
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
 
+    
+    ################Create the plot objects as reactive functions
     make_plot <- reactive({
         
         location <- input$choose_location
@@ -21,11 +23,22 @@ shinyServer(function(input, output, session) {
     })
     
     make_slope_plot <- reactive({
-        location <- input$choose_location
         
+        location <- input$choose_location
+       
         slopes_plot_out <- slopes_plot(df1_small,location)
     })
     
+    make_control_charts <- reactive({
+       # browser()
+        location <- input$choose_location
+        
+        control_charts_out <- control_chart_plots(df1_small,location)
+    })
+    
+   ####################Render the plot objects
+     
+   ################Count Plots###########################
     output$count_plot <- renderPlot({
         req(make_plot())
         
@@ -33,10 +46,64 @@ shinyServer(function(input, output, session) {
         
     })
     
-    output$slope_plot <- renderPlot({
+    ###############Slope Plots###########################
+    output$slope_chart <- renderPlot({
+        
+        req(make_slope_plot())
+     
+        if(make_slope_plot()$message == 'Sufficient data to calculate at least one slope') {
+            
+            print(make_slope_plot()$plot)
+
+        } 
+        
+    })
+    
+    output$slope_chart_tab <- renderUI({
+        
         req(make_slope_plot())
         
-        print(make_slope_plot())
+        if(make_slope_plot()$message == 'Sufficient data to calculate at least one slope') {
+            
+           plotOutput("slope_chart")
+            
+        } else {
+            
+            h5(make_slope_plot()$message)
+        }
+   
+     })
+    
+    ###############Control Charts###########################
+    output$control_chart <- renderPlot({
+        
+        req(make_control_charts())
+        
+        if(make_control_charts()$message == paste0("Sufficient data to display control charts: ",
+                                                    min_n_control_charts," records.")) {
+           
+            print(make_control_charts()$plot)
+            
+        } 
     })
+    
+    output$control_chart_tab <- renderUI({
+         
+        req(make_control_charts())
+        
+        if(make_control_charts()$message == paste0("Sufficient data to display control charts: ",
+                                                   min_n_control_charts," records.")) {
+            
+            plotOutput("control_charts")
+            
+        } 
+        
+        else {
+            
+            h5(make_control_charts()$message)
+        }
+    })
+    
+    ######################End of chart rendering##############
 
 })
